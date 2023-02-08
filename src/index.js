02fda676c3332381a49114374c4e318a1bae881f
usama <usamaaqeel98@gmail.com>
2023-02-08 20:56:02 +0000

    tab.js module created. problems with trying to import. interfering with the insersion of the picture of tea.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import './style.css'; 
 import Icon from './chai.jpg';
+import {homepage, menu, contact} from './tab';
+
 
 console.log('webpack worked! Hello');
 
 function component() {
-    
+    homepage();
+    menu(); 
+    contact();
+
     const home = document.querySelector('.home');
     const tag = document.querySelector('.tag');
-
 
     const myIcon = new Image();
     myIcon.src = Icon;
     myIcon.classList.add('chai-image');
-    home.insertBefore(myIcon, tag)   
+    home.insertBefore(myIcon, tag);
 }
 
 document.body.appendChild(component());
---
+++ src/tab.js
@@ -0,0 +1,11 @@
+export function homepage() {
+    return console.log('you just pressed home...');
+}
+
+export function menu() {
+    return console.log('you just pressed menu...');
+}
+
+export function contact() {
+    return console.log('you just pressed contact...');
+}
