332bfa1fa5cec7ef9a009b6e515953a2bece5525
usama <usamaaqeel98@gmail.com>
2023-02-11 01:05:34 +0000

    tabs generate information using DOM. Contact information added. color scheme of main page fixed. slide animation added when tabs clicked

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
+import MenuItem, {homepage, menu, contact} from './tab';
 import './style.css'; 
-import Icon from './chai.jpg';
-import {homepage, menu, contact} from './tab';
 
+const homeButton = document.querySelector('.home-button');
+const menuButton = document.querySelector('.menu-button');
+const contactButton = document.querySelector('.contact-button');
+
+homeButton.addEventListener('click', homepage);
+menuButton.addEventListener('click', menu);
+contactButton.addEventListener('click', contact);
 
 console.log('webpack worked! Hello');
-
-function component() {
-    homepage();
-    menu(); 
-    contact();
-
-    const home = document.querySelector('.home');
-    const tag = document.querySelector('.tag');
-
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    myIcon.classList.add('chai-image');
-    home.insertBefore(myIcon, tag);
-}
-
-document.body.appendChild(component());
--- src/tab.js
+++ src/tab.js
@@ -1,11 +1,95 @@
+import Icon from './chai.jpg';
+
+export default class MenuItem {
+    
+    constructor(item, price) {
+        this.item = item; 
+        this.price = price;
+    }
+
+    constructString(item, price) {
+        console.log(`${this.item} is ${this.price}`);
+    }
+}
+
 export function homepage() {
-    return console.log('you just pressed home...');
+    console.log('you just pressed home...');
+    
+    const page = document.querySelector('.page');
+    page.innerHTML = "";
+
+    const title = document.createElement('div');
+    title.textContent = 'Home';
+    title.classList.add('title');
+    title.classList.add('slide');
+    
+    const infoContainer = document.createElement('div');
+    infoContainer.textContent = 'Welcome to ChaiShai, aiwjbfvibsd naos ifhvpoiqhiohq woh reh giuhriugh vu hgoih epough vopiaehsov nkjdbv oirhpihdvoiajslkm oisdhoi jergfh [ioe c lvkn oisgh voi[a sdgoh eoihg voislvkn gohg[oih oihg oi gh oidhg oih doighoshg oeh ogih eapiough voiuehoyfg euydh kjbjkv poih guipvhe odghv oiehf ovhfghohg[oieh gopih oh ogh oeauih ';
+    infoContainer.classList.add('info');
+    infoContainer.style.cssText = "display: flex;";
+    
+    page.appendChild(title);
+    page.appendChild(infoContainer);
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    myIcon.classList.add('chai-image');
+    infoContainer.appendChild(myIcon);
 }
 
 export function menu() {
-    return console.log('you just pressed menu...');
+    const page = document.querySelector('.page');
+    page.innerHTML = "";
+    console.log('you just pressed menu...');
+
+    const title = document.createElement('div');
+    title.textContent = 'Menu';
+    title.classList.add('title');
+    title.classList.add('slide');
+
+    page.appendChild(title);
+
 }
 
 export function contact() {
-    return console.log('you just pressed contact...');
+    const page = document.querySelector('.page');
+    page.innerHTML = "";
+    
+    const title = document.createElement('div');
+    title.textContent = 'Contact';
+    title.classList.add('title');
+    title.classList.add('slide');
+    page.appendChild(title);
+
+    const infoContainer = document.createElement('div');
+    const invitation = document.createElement('div');
+    const addressContainer = document.createElement('div');
+    const phoneContainer = document.createElement('div');
+    const emailContainer = document.createElement('div');
+    
+    invitation.textContent = "If you like what you see here, pop down for a cuppa!"
+    addressContainer.textContent = " Address: 32 Restaurant Road, Soho, London, W1 2GL";
+    phoneContainer.textContent = "Phone: 020 356 785";
+    emailContainer.textContent = "Email: chaishai@tea.co.uk";
+    
+    infoContainer.classList.add('info-container');
+    invitation.classList.add('info');
+    invitation.classList.add('invite');
+    invitation.classList.add('slide');
+    addressContainer.classList.add('info');
+    addressContainer.classList.add('slide');
+    phoneContainer.classList.add('info');
+    phoneContainer.classList.add('slide');
+    emailContainer.classList.add('info');
+    emailContainer.classList.add('slide');
+    
+    infoContainer.appendChild(invitation);
+    infoContainer.appendChild(addressContainer);
+    infoContainer.appendChild(phoneContainer);
+    infoContainer.appendChild(emailContainer);
+    page.appendChild(infoContainer); 
+
+
+    console.log('you just pressed contact...');
 }
+
+homepage(); 
